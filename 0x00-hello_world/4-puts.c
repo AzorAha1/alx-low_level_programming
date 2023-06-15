2f54b52217d77d09dc6a44da3d483992213fc972
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-15 16:08:37 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ * my function is to print out a string using puts instead of printf
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle,");
+	puts('"Programming is like building a multilingual puzzle');
 	return (0);
 }
