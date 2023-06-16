1060dcc18efbb1f01bd6b8a26c03ede5eeae0b4d
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-16 14:58:03 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - This is a description
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	char alpha;
+	
+	for(alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	
+	putchar('\n');
+
+	return(0);
+}
+
