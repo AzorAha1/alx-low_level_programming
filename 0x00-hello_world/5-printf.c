9475f8b33dbfc4a2018f284b6055c292c7812338
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-15 17:42:14 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *  main - This is a description
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
