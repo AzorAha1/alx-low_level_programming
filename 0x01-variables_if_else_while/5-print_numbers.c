4f3802256eadeecbfe771923386db0d0326a8d0d
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-16 15:56:55 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - This is a description
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+	int num = 0;
+
+	while(num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	
+	printf("\n");
+	return (0);
+
+}
