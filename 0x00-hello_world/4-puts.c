028fae571e78af2589f99c84d10d26b3543a1f2e
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-15 17:36:09 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  main - This is a description
  *
@@ -9,4 +8,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
+
