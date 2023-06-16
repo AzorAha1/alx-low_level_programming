f6cd05ecbfa8788dee4a06b5c9a7f38fea71ed53
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-15 18:15:20 -0700

    sizes of data types in 64bit machines vs 32 bits machines

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *  main - This is a description
+ *
+ *  Return: Always (Success)
+ */
+int main(void)
+{
+        printf("Size of char: %lu byte(s)\n",(unsigned long)sizeof(char));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
+	printf("Size of long long int: %lu byte(s)\n",(unsigned long)sizeof(long long int));
+ 	return (0);
+}
