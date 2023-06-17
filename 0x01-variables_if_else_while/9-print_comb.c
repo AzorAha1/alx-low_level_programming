ec0d955b40c9731646ff8ff657d9dafb31ff1f8b
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-16 18:13:41 -0700

    all single digit base 10 combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-        int num = 0;
+	int num = 0;
 
-        while (num < 10)
-        {
-                putchar('0' + num);
-		if(num < 9)
+	while (num < 10)
+	{
+		putchar('0' + num);
+		if (num < 9)
 		{
 			putchar(',');
 		}
 		putchar(' ');
-                num++;
-        }
-	
-        putchar('\n');
-        return (0);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 
 }
+
