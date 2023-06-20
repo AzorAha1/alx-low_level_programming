be061fa98fb299c5197358a3c3629786bbb25bbc
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-20 14:02:16 -0700

    print 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alphax10;
+
+	for (alphax10 = 0; alphax10 < 10; alphax10++)
+	{
+		_putchar(alphax10);
+	}
+	printf("\n");
+}
