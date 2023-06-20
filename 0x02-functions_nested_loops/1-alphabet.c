711feea2f43a5b033f80ed4c2d102920425efbef
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-20 11:18:01 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "_putchar.c"
+#include "main.h"
 /**
  *  main - This is a description
  *
@@ -8,7 +9,7 @@
 void print_alphabet(void);
 int main(void)
 {
-	void print_alphabet();
+	 print_alphabet();
 	_putchar('\n');
 	return (0);
 }
