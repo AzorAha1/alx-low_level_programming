0de0b53301f4c8a20523a9cd46fe8d9965c46d61
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-20 16:50:23 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,15 +12,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(1, '+');
+		printf("+");
+		return (1);
 	}
 	if (n == 0)
 	{
-		_putchar(0, '0');
+		printf("0");
+		return (0);
 	}
 	if (n < 0)
 	{
-		_putchar(-1, '-');
+		print("-");
+	       return (-1);	
 	}
 	return (0);
 }
