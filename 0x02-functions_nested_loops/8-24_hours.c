68b3eaac89cc32b98cb5f2bf8a1827a10c29a018
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-21 11:07:38 -0700

    timer 24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,17 +8,18 @@
  * Description - this is the description
  * Return: Always 0 (Success)
  */
-void jack_bauer(void) {
-    int minute = 0;
-    int hour = 0;
-	
+void jack_bauer(void)
+{
+	int minute = 0;
+	int hour = 0;
+
 	while (hour < 24)
 	{
-	while (minute <= 59)
-	{
-		printf("%02d:%02d\n", hour, minute);
-		minute++;
-	}
+		while (minute <= 59)
+		{
+			printf("%02d:%02d\n", hour, minute);
+			minute++;
+		}
 		minute = 0;
 		hour++;
 	}
