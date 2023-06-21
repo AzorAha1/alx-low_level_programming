af5dee411924f6d0fa00cf6f16693f0e97dd9296
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-21 14:41:35 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - print last digit of a  number.
+ * @void: takes nothing
+ *
+ * Description - this is the description
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			printf("%d, ", k);
+		}
+		printf("\n");
+	}
+}
