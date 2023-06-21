591ce5321080371c1f66056846d23168e5401606
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-21 15:40:54 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - print last digit of a  number.
+ * @a: takes input
+ * @b: takes iput
+ * Description - this is the description
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+
+}
