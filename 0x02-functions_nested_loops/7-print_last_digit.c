0336a01c5cf832ef2e7f5b23e43642defe1d928c
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-21 06:48:00 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,9 +11,9 @@
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
-	if (last_digit < 0)
+	if (n < 0)
 	{
-		last_digit = -lastdigit;
+		n = n*-1;
 	}
 	_putchar('0' + last_digit);
 	return (last_digit);
