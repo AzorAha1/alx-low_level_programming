4dd20fac96624aa9ddd78a162574c3e7285cd77b
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-21 09:31:26 -0700

    24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a  number.
+ * @n: takes input
+ *
+ * Description - this is the description
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void) {
+    int i;
+    
+    for(i = 0; i <= 1440; i++){
+        printf("%d\n", i);
+    }
+}
