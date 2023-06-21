6b728197e37be3a0dc2b0519f0db89fbb46e324a
Mohammed Faisal Adam <@wizfaiz@icloud.com>
2023-06-20 18:03:38 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * int_abs - print sign of number.
+ * @n: takes input
+ *
+ * Description - this is the description
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	absolute = abs(n)
+	return (0);
+}
+
