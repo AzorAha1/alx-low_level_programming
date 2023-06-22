96581dbec0cc3eee116627a090c538d20ed505f6
Azor <wizfaiz@icloud.com>
2023-06-22 12:10:09 +0100

    .\11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,10 +2,8 @@
 
 /**
  * print_to_98 - print to 98
- * @n: takes input 
- * 
+ * @n: takes input
  * Description - this is the description
- * 
  * Return: Always 0 (success)
 */
 void print_to_98(int n)
