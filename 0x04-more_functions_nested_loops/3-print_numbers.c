e438b3e26f9247df1c2301bad7c80e755ac07ae1
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-24 18:15:43 +0100

    printing 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - print from 0-9
+ * @void: takes nothing
+ * Description - this is the description
+ * Return: return value type
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+}
