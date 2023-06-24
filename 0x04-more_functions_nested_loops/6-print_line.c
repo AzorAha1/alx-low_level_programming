8e733e696704b6d4e0f2fc5a8a9ccbb05404b709
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-24 23:29:57 +0100

    straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ * @n: takes int n
+ * Description - this is the description
+ * Return: return value type
+ */
+void print_line(int n)
+{
+	int i = 0;
+	char c = '_';
+
+	if (n <= 0)
+	{
+		printf("\n");
+	}
+	else
+	{
+
+		for (i; i < n; i++)
+		{
+			printf("%c", c);
+		}
+	}
+}
