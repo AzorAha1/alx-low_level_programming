858e845a5b1c279904edc1a55557d34433c14e04
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-24 17:31:53 +0100

    checking for uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase.
+ * @c: takes input
+ *
+ * Description - this is the description
+ * Return: Always 0 (Success)
+ */
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
