da475e3706283f076aa787ed959f648f470a54d1
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-24 20:56:59 +0100

    print 0-14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print 10 times the numbers
+ * @void: takes nothing
+ * Description - this is the description
+ * Return: return value type
+ */
+void more_numbers(void)
+{
+	int i;
+	int a;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for(a = 0; a <= 14; a++)
+		{
+			printf("%d", a);
+		}
+		printf("\n");
+	}
+}
