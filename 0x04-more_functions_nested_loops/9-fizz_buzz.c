bef21038965df269fa6062b9d27304f47d2bef8c
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-25 22:18:53 +0100

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - fizzbuzz
+ * @void: takes nothing
+ * Description - this is the description
+ * Return: return value type
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; ++i)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
