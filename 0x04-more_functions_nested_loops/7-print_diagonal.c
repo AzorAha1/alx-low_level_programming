3f908ac4c1dab9ffc65d7545617da02f13fd8780
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-25 01:20:03 +0100

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonally
+ * @n: takes int n
+ * Description - this is the description
+ * Return: return value type
+ */
+void print_diagonal(int n)
+{
+        if (n <= 0)
+        {
+                _putchar('\n');
+        }
+        else
+        {
+                int i;
+
+                for (i = 1; i <= n; i++)
+                {
+                        _putchar('\\');
+                }
+                _putchar('\n');
+        }
+}
+
