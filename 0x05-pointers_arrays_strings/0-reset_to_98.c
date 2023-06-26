9dec89263048f20e4ec8b72e733d75dc39a9b70f
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-26 22:08:05 +0100

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset to 98
+ * @n: takes int size
+ * Description - this is the description
+ * Return: return value type
+ */
+void reset_to_98(int *n)
+{
+	int value = &n;
+
+	value =  98;
+}
