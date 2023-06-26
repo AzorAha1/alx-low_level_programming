829e7ebf4a552677571c9464cd95fd2204ef0488
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-26 23:33:45 +0100

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - reset to 98
+ * @a: takes int size
+ * @b: takes int
+ * Description - this is the description
+ * Return: return value type
+ */
+int _strlen(char *s)
+{
+	int i = strlen(*s);
+	
+	return (i);
+}
