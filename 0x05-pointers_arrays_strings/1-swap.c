0da55923ab00f0e3d1a18be12c991a03cc905192
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-26 22:41:41 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - reset to 98
+ * @a: takes int size
+ * @b: takes int
+ * Description - this is the description
+ * Return: return value type
+ */
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
