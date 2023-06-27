0312683fa4062dd6e97b14eccc477bb030564907
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 22:55:17 +0100

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * print_array - prints half
+ * @a: takes int
+ * @n: takes int
+ * Description - this is the description
+ * Return: return value type
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return dest;
+}
