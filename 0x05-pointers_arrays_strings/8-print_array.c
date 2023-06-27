0de2a571beedd3e0c57f0098d0a11912dc7860fd
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 22:16:31 +0100

    print n elements in an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - prints half
+ * @str: char str
+ * Description - this is the description
+ * Return: return value type
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+}
