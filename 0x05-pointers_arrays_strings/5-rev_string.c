f45bbb0fa0895094accd0077a1b01b756b291a75
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 17:04:25 +0100

    reversed string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,20 +2,23 @@
 #include "main.h"
 #include <string.h>
 /**
- * print_rev - getting string length
+ * rev_string - reverse string
  * @s: char s
  * Description - this is the description
  * Return: return value type
  */
+
 void rev_string(char *s)
 {
-	int l,c,i;
+	int l;
+	char c;
+	int i;
 
 	l = strlen(s);
-	for (i = 0; i <= l/2; i++)
+	for (i = 0; i <= l / 2; i++)
 	{
 		c = s[i];
-		s[i] = s[l-1-i];
-		s[l-1-i] = c;
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = c;
 	}
 }
