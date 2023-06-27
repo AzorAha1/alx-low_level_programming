b37bcf49b9594619f066bb79b97a401d642b5cdf
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 14:36:08 +0100

    printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,13 +2,23 @@
 #include "main.h"
 #include <string.h>
 /**
- * _puts - getting string length
- * @str: char s
+ * print_rev - getting string length
+ * @s: char s
  * Description - this is the description
  * Return: return value type
  */
 void print_rev(char *s)
 {
-	char reversed = strrev(s);
-	puts(reversed);	
+	int i;
+	int l;
+	char c;
+
+	l = strlen(s);
+	for (i = 0; i < l/2; i++)
+	{
+		c = s[i];
+		s[i] = s[l-1-i];
+		s[l-1-i] = c;
+	}
+	printf("%s", s);
 }
