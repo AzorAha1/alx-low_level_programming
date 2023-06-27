91f3fb993b182eae5712f6a653cfe8efe6b0cc3c
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 01:44:35 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - getting string length
+ * @str: char s
+ * Description - this is the description
+ * Return: return value type
+ */
+void print_rev(char *s)
+{
+	puts(strev(str));
+}
