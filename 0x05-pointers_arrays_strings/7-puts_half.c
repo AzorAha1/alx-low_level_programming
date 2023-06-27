785593540e3ca30553fad6b22dfcf710eae5c284
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 21:29:21 +0100

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,21 +12,13 @@ void puts_half(char *str)
 	int i;
 	int l;
 	int h;
-	int n;
 
 	l = strlen(str);
-	n = l - 1 / 2;
 	h = l / 2;
-	for (i = h; i > l; i++)
+	for (i = h; i < l; i++)
 	{
-		if (l % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		else
-		{
-			_putchar(str[n]);
-		}
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 
 }
