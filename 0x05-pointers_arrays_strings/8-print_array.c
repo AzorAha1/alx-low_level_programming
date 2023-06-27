c0afafca9ed5aae3c53a9e1c09ff97d725514219
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 22:26:46 +0100

    printing array of n numbers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,17 +2,18 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts_half - prints half
- * @str: char str
+ * print_array - prints half
+ * @a: takes int
+ * @n: takes int
  * Description - this is the description
  * Return: return value type
  */
 void print_array(int *a, int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
-	{	
+	{
 		printf("%d", a[i]);
 		if (i < n - 1)
 		{
