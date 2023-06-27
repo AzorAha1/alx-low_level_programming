c05e860f92ac1c10a359d13e0462b91d0f5a325b
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 14:40:00 +0100

    printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,14 +12,13 @@ void print_rev(char *s)
 	int i;
 	int l;
 	char c;
-	char str;
 
 	l = strlen(s);
 	for (i = 0; i < l/2; i++)
 	{
 		c = s[i];
 		s[i] = s[l-1-i];
-		str[l-1-i] = c;
+		s[l-1-i] = c;
 	}
 	printf("%s", s);
 }
