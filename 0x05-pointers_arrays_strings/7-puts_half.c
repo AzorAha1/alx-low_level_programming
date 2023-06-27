0e7cc0d87f07f32baf08bf4e7490443f71abab5f
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-27 21:40:24 +0100

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,8 +2,8 @@
 #include "main.h"
 #include <string.h>
 /**
- * puts2 - print every other character
- * @str: char s
+ * puts_half - prints half
+ * @str: char str
  * Description - this is the description
  * Return: return value type
  */
@@ -12,12 +12,21 @@ void puts_half(char *str)
 	int i;
 	int l;
 	int h;
+	int n;
 
 	l = strlen(str);
+	n = l - 1 / 2;
 	h = l / 2;
 	for (i = h; i < l; i++)
 	{
-		_putchar(str[i]);
+		if (l % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			_putchar(str[n]);
+		}
 	}
 	_putchar('\n');
 
