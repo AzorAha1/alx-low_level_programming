afb9afe6b7a13d1699c0a46376062867630e0812
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-29 21:17:15 +0100

    strncat function

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * _strncat - function to concatenate two strings with the use of n
+ * @dest: this takes char
+ * @src: this takes char
+ * @n: this takes int
+ * Description - this function concatenates two strings, its different from strcat because you cantake n
+ * Return: this this the return type
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
