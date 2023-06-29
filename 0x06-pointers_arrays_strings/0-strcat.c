2ce80c86b8deef1b73054e6016dc915f203e3231
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-29 21:02:16 +0100

    concatanation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * _strcat - function to concatenate
+ * @dest - takes char
+ * @src - takes char
+ * Description - this is the description
+ * Return - this is the return type
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
