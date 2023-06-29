e1b5dba378a5f3824351e9af460933956804bf98
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-29 21:20:04 +0100

    strncat function

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * @dest: this takes char
  * @src: this takes char
  * @n: this takes int
- * Description - this function concatenates two strings, its different from strcat 
+ * Description - this function concatenates two strings
  * Return: this this the return type
  */
 char *_strncat(char *dest, char *src, int n)
