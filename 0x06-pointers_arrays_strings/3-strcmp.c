9e32d21e01620a27e3aef2fb6ae3ad71f1b6fe05
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-29 23:07:50 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 {
 	if (strcmp(s1, s2) < 0)
 	{
-		return (-15);
+		return (15);
 	}
 	else if (strcmp(s1, s2) > 0)
 	{
-		return (15);
+		return (-15);
 	}
 	else if (strcmp(s1, s2) == 0)
 	{
