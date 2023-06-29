b4c8de8d77ddf7304eef3a296a20c7d463148d15
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-29 23:34:49 +0100

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (s1 && (s1 == s2))
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[1] != '\0'; i++)
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
-	return (s1 - s2);
+	return (s1[i] - s2[i]);
 }
