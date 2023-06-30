1b51017779152d82568a8ee94b1f6bbb5774bcf2
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-30 14:55:25 +0100

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 /**
- * _strcmp - function to concatenate two strings with the use of n
- * @s1: this takes char
- * @s2: this takes char
- * Description - strcmp
+ * reverse_array - function to concatenate two strings with the use of n
+ * @a: this takes int
+ * @n: this takes int
+ * Description - reverse array
  * Return: this this the return type
  */
 void reverse_array(int *a, int n)
@@ -12,7 +12,7 @@ void reverse_array(int *a, int n)
 	int i;
 	char c;
 
-	for(i = 0; i <= n/2; i++)
+	for (i = 0; i <= n / 2; i++)
 	{
 		c = a[i];
 		a[i] = a[n - 1 - i];
