25dc6a0dded0dc8023247c320fc551d550b10c66
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-30 20:31:42 +0100

    to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+/**
+ * reverse_array - function to concatenate two strings with the use of n
+ * @a: this takes int
+ * @n: this takes int
+ * Description - reverse array
+ * Return: this this the return type
+ */
+char *string_toupper(char *p)
+{
+	int i;
+
+	for (i = 0; p[i] != '\0'
+}
