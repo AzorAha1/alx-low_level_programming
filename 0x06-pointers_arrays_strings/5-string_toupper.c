5db78da6336f894cc7f5591143d38b75399d1e8b
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-06-30 20:56:17 +0100

    upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,15 +2,21 @@
 #include<string.h>
 #include<ctype.h>
 /**
- * reverse_array - function to concatenate two strings with the use of n
- * @a: this takes int
- * @n: this takes int
- * Description - reverse array
+ * string_toupper - function to print upper case
+ * @p: this takes char
+ * Description - uppercase
  * Return: this this the return type
  */
 char *string_toupper(char *p)
 {
 	int i;
 
-	for (i = 0; p[i] != '\0'
+	for (i = 0; p[i] != '\0'; i++)
+	{
+		if (p[i] >= 97 && p[i] <= 122)
+		{
+			p[i] = toupper(p[i]);
+		}
+	}
+	return (p);
 }
