6e7b7a2f2c94df2c5eae65998003942140f36246
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-03 22:29:55 +0100

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strspn - strchr function
+ * @s: takes char
+ * @accept: takes char
+ * Description - function
+ * Return: return type
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int pos = strspn(s, accept);
+	return (pos);
+}
