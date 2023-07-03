1933133d259ec573f6646907fbb4f15e93291015
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-03 22:07:30 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strchr - strchr function
+ * @s: takes char
+ * @c: takes char
+ * Description - function
+ * Return: return type
+ */
+char *_strchr(char *s, char c)
+{
+	char str = strchr(s, c);
+	if (str)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
