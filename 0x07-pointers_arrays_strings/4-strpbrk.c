6e04052c751dba48f03839c2583f8b3da348ca3e
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-03 23:27:53 +0100

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strpbrk - strchr function
+ * @s: takes char
+ * @accept: takes char
+ * Description - function
+ * Return: return type
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	if (s == NULL || accept == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				return &s[i];
+			}
+		}
+	}
+	return (s);
+}
