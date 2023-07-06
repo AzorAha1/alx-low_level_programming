b3df0ff6ccd4cb7864b7213028fef06f67e06911
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-06 19:49:20 +0100

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print string - print string
+ * @s: takes char
+ * Description - function
+ * Return: return type
+ */
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
