601abd2596d99462f68dbdc77eb6e344bcb2c5c0
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-06 21:32:03 +0100

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * factorial - factorial
+ * @n: takes int
+ * Description - function
+ * Return: return type
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return factorial (n * (n - 1));
+	}
+}
