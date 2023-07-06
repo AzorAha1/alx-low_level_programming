91cd55776f18f732b1f317f22cb6517b1bacfa8e
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-06 23:35:42 +0100

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _pow_recursion - pow
+ * @x: takes int
+ * @y: takes int
+ * Description - function
+ * Return: return type
+ */
+int _pow_recursion(int x, int y)
+{
+	int result;
+
+	result = pow(x, y);
+	return (result);
+}
