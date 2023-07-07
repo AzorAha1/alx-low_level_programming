dd26776ef9758a8297d275a65be0a31302c9ea26
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-07 23:03:01 +0100

    main corrected

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,14 @@
 #include "main.h"
+/**
+ * main - function
+ * @void: takes empty
+ * Description - descriprion
+ * Return: return type
+ */
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n
+	\t-Kevin Mitnick");
+	return (0);
 }
