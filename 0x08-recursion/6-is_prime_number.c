62811ee1dd614d8c84758b9a60d6a23cd1683c5b
vboxuser <vboxuser@Faisal.myguest.virtualbox.org>
2023-07-07 01:46:28 +0100

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+ * isprime - prime number
+ * @number: takes int
+ * @prime: takes prime
+ * Description - function
+ * Return: return type
+ */
+int isprime(int number, int prime)
+{
+	if (number <= 1)
+	{
+		return (-1);
+	}
+	else if (number % prime == 0)
+	{
+		return (0);
+	}
+	else 
+	{
+		return isprime(number, prime + 1);
+	}
+}
+/**
+ * is_prime_number - prime
+ * @n: int n
+ * Description - function
+ * Return: return type
+ */
+int is_prime_number(int n)
+{
+	return isprime(n, 2);
+}
