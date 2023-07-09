9d92d81971b89b6a7ece46d5a9261ec7c4900b32
AzorAha1 <wizfaiz@icloud.com>
2023-07-09 19:09:15 +0100

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - print string
+ * @argc: argument count
+ * @argv: argument vector
+ * Description - function
+ * Return: return type
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
