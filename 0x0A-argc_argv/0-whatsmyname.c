a5f777832e95e602e9904ff9e12eee4f1d66a2df
AzorAha1 <wizfaiz@icloud.com>
2023-07-09 19:10:53 +0100

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strlen_recursion - print string
+ * main - print string
  * @argc: argument count
  * @argv: argument vector
  * Description - function
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
