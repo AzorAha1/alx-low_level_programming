f74392a7f18ee10c8a5e6c0461beac13b70d4e69
AzorAha1 <wizfaiz@icloud.com>
2023-07-11 22:34:02 +0100

    con

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - function name
+ * @str: takes char
+ * Description - this is the description
+ * Return: return type
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *p;
+	size_t s1len;
+	size_t s2len;
+	size_t total;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	s1len = strlen(s1);
+	s2len = strlen(s2);
+	total = s1len + s2len; 
+	p = (char *)malloc(total + 1 * sizeof(char));
+	strcat(p, s1);
+	strcat(p, s2);
+	return (p);
+}
