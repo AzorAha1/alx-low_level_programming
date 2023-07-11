1d134ba6827612f657c390cccac486e2bfaddfb7
AzorAha1 <wizfaiz@icloud.com>
2023-07-12 00:58:31 +0100

    2 dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 	}
 	p = (int **)malloc(height * sizeof(int *));
 	if (p == NULL)
-	{	free(p);
+	{	free(grid[p]);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
