dc40ca0d6806fc56ae66b28758a2f0e6229666f7
AzorAha1 <wizfaiz@icloud.com>
2023-07-12 01:07:49 +0100

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - function name
+ * @width: takes int
+ * @height: takes char
+ * Description - this is the description
+ * Return: return type
+ */
+free free_grid(int **grid, int height)
+{
+	int i;
+	int j;
+	int **p;
+	
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	p = (int **)malloc(height * sizeof(int *));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = (int *)malloc(width * sizeof(int));
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			free(grid[j]);
+			p[i][j] = 0;
+		}
+	}
+	free(p);
+}
+
