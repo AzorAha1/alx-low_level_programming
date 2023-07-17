26053f8878047f4cbf97ab7fcdf9463c2b5a6ad4
AzorAha1 <wizfaiz@icloud.com>
2023-07-18 00:02:53 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - name of struct
+ * @name: takes char
+ * @age: takes float
+ * @owner: takes char
+ * Description: this is the description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
