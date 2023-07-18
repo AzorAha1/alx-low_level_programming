60177dfebbe4bf1e346f5fbcdb791f432fe52623
AzorAha1 <wizfaiz@icloud.com>
2023-07-18 03:52:12 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,18 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	p->name = strdup(name);
-	if (p->name == NULL)
-	{
-		free(p);
-		return (NULL);
-	}
 	p->age = age;
 	p->owner = strdup(owner);
-	if (p->owner == NULL)
-	{
-		free(p->name);
-		free(p);
-		return (NULL);
-	}
 	return (p);
 }
