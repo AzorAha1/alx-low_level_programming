984cc004be064d07d78a74e22b91dcd77cd623dd
AzorAha1 <wizfaiz@icloud.com>
2023-07-18 01:00:47 +0100

    1-init_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - name
+ * Description - description
+ * Return: return type
+ * struct dog - name
+ * Description: description
+ * @d: dog
+ */
+void print_dog(struct dog *d)
+{
+	if 
+}
