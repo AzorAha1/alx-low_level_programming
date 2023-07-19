43f58d40ab51625b82fea67e54e0c7f75afa2755
AzorAha1 <wizfaiz@icloud.com>
2023-07-20 00:16:42 +0100

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - function name
+ * @array: takes int pointer
+ * @size: size_t
+ * @action: function pointer
+ * Description - this is the description
+ * Return: return type
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	array = (int *)malloc(size);
+	action(*array);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,5 @@
 #ifndef PRINT_NAME
 #define PRINT_NAME
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
