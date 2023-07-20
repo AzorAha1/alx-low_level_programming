e599b919867940c6aaea6660b77c8f5eac21ec26
AzorAha1 <wizfaiz@icloud.com>
2023-07-21 00:50:33 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,6 +3,65 @@
 #include <stdarg.h>
 #include <string.h>
 /**
+ * print_char - function name
+ * @arguments: takes arguments
+ * Description - this is the description
+ * Return: return type
+ */
+void print_char(va_list arguments)
+{
+	char c;
+
+	c = va_arg(arguments, int);
+	printf("%c", c);
+}
+/**
+ * print_int - function name
+ * @arguments: takes arguments
+ * Description - this is the description
+ * Return: return type
+ */
+void print_int(va_list arguments)
+{
+	int i;
+
+	i = va_arg(arguments, int);
+	printf("%d", i);
+}
+/**
+ * print_double - function name
+ * @arguments: takes arguments
+ * Description - this is the description
+ * Return: return type
+ */
+void print_double(va_list arguments)
+{
+	double flo;
+
+	flo = va_arg(arguments, double);
+	printf("%f", flo);
+}
+/**
+ * print_string - function name
+ * @arguments: takes arguments
+ * Description - this is the description
+ * Return: return type
+ */
+void print_string(va_list arguments)
+{
+	char *ch;
+
+	ch = va_arg(arguments, char*);
+	if (ch == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", ch);
+	}
+}
+/**
  * print_all - function name
  * @format: takes cahr
  * Description - this is the description
@@ -13,10 +72,6 @@ void print_all(const char * const format, ...)
 	va_list arguments;
 	int format_l;
 	int i;
-	char c;
-	int integer;
-	double flo;
-	char *ch;
 
 	i = 0;
 	va_start(arguments, format);
@@ -25,31 +80,19 @@ void print_all(const char * const format, ...)
 	{
 		if (format[i] == 'c')
 		{
-			c = va_arg(arguments, int);
-			printf("%c", c);
+			print_char(arguments);
 		}
 		else if (format[i] == 'i')
 		{
-			integer = va_arg(arguments, int);
-			printf("%d", integer);
+			print_int(arguments)
 		}
 		else if (format[i] == 'f')
 		{
-			flo = va_arg(arguments, double);
-			printf("%f", flo);
+			print_double(arguments);
 		}
 		else if (format[i] == 's')
 		{
-			ch = va_arg(arguments, char*);
-
-			if (ch == NULL)
-			{
-				printf("(nil)");
-			}
-			else
-			{
-				printf("%s", ch);
-			}
+			print_string(arguments);
 		}
 		i++;
 	}
