0c9a16d83624375de2ba6311a7d57ab1d56aa394
AzorAha1 <wizfaiz@icloud.com>
2023-07-20 21:51:11 +0100

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 /**
  * array_iterator - function name
  * @array: takes int pointer
@@ -8,4 +9,18 @@
  * Description - this is the description
  * Return: return type
  */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
 
+	va_list arguments;
+	va_start(arguments, n);
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(arguments, unsigned int);
+		sum += x;
+	}
+	va_end(arguments);
+	return (sum);
+}
