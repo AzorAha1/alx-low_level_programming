4b850071adc1754a1c487c3a2e9a0b09077b42f5
AzorAha1 <wizfaiz@icloud.com>
2023-07-20 01:45:48 +0100

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,7 @@ int cmp(int i)
 	{
 		return (1);
 	}
-	if (i <=0 )
-	{
-		return (-1);
-	}
-	return (i);
+	return (-1)
 }
 /**
  * int_index - function name
@@ -30,6 +26,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0)
+	{
+		return (-1);
+	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
