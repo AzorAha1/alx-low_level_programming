4f5757366f8d5493810bd2111a0b6ebe64e9445d
AzorAha1 <wizfaiz@icloud.com>
2023-07-20 21:32:44 +0100

    added

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_iterator - function name
+ * @array: takes int pointer
+ * @size: size_t
+ * @action: function pointer
+ * Description - this is the description
+ * Return: return type
+ */
+
