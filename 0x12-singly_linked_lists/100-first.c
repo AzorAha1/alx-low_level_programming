c7652b2e424adcaa687e06d7ac9cc4a3be6d436e
AzorAha1 <wizfaiz@icloud.com>
2023-07-28 22:04:39 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * printsomething - function
+ * @void: nothing
+ * Description - this is the description
+ * Return: void
+ */
+void printsomething(void)
+{
+	printf("You're beat! and yet, you much allow,\nI bore my house upon my back!\n");
+}
