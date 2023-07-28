d50a47facfe9e2fab9ab148e951d27601f649703
AzorAha1 <wizfaiz@icloud.com>
2023-07-29 00:19:35 +0100

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-	text db "Hello, Holberton", 0xA ;
+	text db 'Hello, Holberton', 0xA ;
 	text_length equ $ - text
 
 section .text
 	global _start
 
 _start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, text
-	mov rdx, text_length
+	mov eax, 1
+	mov edi, 1
+	mov esi, text
+	mov edx, text_length
 	syscall
 
-	mov rax, 60
-	xor rdi, rdi
+	mov eax, 60
+	xor edi, edi
 	syscall
