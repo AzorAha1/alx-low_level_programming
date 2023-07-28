9084862a108e1bd051deae2264bc7116f463e52e
AzorAha1 <wizfaiz@icloud.com>
2023-07-28 02:19:50 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,8 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *newnode = malloc(sizeof(list_t));
+	list_t *newnode;
+	newnode = malloc(sizeof(list_t));
 
 	if (str == NULL)
 	{
