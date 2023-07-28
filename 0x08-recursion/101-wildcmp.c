814ea33a5f7dd904f9a589a4e40a2de2b9716d4b
AzorAha1 <wizfaiz@icloud.com>
2023-07-28 16:49:27 +0100

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,9 +12,8 @@ int wildcmp_finder(char *string1, char *string2)
 {
 	int checker;
 	char *special_char = strchr(string2, '*');
-	
-	checker = strcmp(string1, string2);
 
+	checker = strcmp(string1, string2);
 	if (checker == 0)
 	{
 		return (1);
