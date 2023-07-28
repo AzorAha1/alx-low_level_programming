02bf1a625ab002a1cf5ad7f78d37d80a4bb3322d
AzorAha1 <wizfaiz@icloud.com>
2023-07-28 01:05:32 +0100

    node insertion

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - name
+ * @head: parameter
+ * @str:str
+ * Description - this is the description
+ * Return: return type
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+
+	newnode->next = *head;
+	*head = newnode;
+
+	return (newnode);
+}
