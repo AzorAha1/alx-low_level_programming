1fd40ffbb4997261d00fe873eafbb83ae8b8f96a
AzorAha1 <wizfaiz@icloud.com>
2023-07-30 22:55:15 +0100

    strtok

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -10,19 +10,36 @@
 char **strtow(char *str)
 {
 	char *dub_str;
-	char *sep_str;
-
+	char *str_piece;
+	int counter;
+	char **alloc_words;
+	int i;
+	
+	i = 0;
+	counter = 0;
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	dub_str = strdup(str);
-	sep_str = strtok(dub_str, " ");
-
-	while (sep_str != NULL)
+	while (str_piece != NULL)
 	{
-		sep_str = strtok(NULL, " ");
-		printf("%s\n", sep_str);
+		counter++;
+		str_piece = strtok(NULL, " ");
 	}
-	return (0);
+	alloc_words = (char **)malloc(counter * sizeof(char *));
+	dub_str = strdup(str);
+	str_piece = strtok(dub_str, " ");
+
+	while (str_piece != NULL)
+	{
+		alloc_words[i] = strdup(str_piece);
+		str_piece = strtok(NULL, " ");
+		i++;
+	}
+	for (i = 0; i < counter; i++)
+	{
+		printf("%s\n" , alloc_words[i]);
+		str_piece = strtok(NULL, " ");
+	}
+	return (alloc_words);
 }
