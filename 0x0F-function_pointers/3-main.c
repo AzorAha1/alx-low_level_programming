48e254a55ad3587fa8e579c7367227309ee91bb3
AzorAha1 <wizfaiz@icloud.com>
2023-07-30 02:29:48 +0100

    operator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	else if (arith != '+' || arith != '-' || arith != '*' ||
-		arith != '/' || arith != '%')
+	else if (*arith != '+' || *arith != '-' || *arith != '*' ||
+		*arith != '/' || *arith != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	else if (arith == '/' || arith == '%' && second_input == 0)
+	else if (*arith == '/' || *arith == '%' && second_input == 0)
 	{
 		printf("Error\n");
 		exit(100);
