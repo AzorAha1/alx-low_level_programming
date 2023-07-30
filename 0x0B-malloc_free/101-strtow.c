a48a28ccf190dd4c943e0511dd6b4324a0a43610
AzorAha1 <wizfaiz@icloud.com>
2023-07-30 23:00:01 +0100

    strtow using strtok

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,35 +11,18 @@ char **strtow(char *str)
 {
 	char *dub_str;
 	char *str_piece;
-	int counter;
-	char **alloc_words;
-	int i;
-	
-	i = 0;
-	counter = 0;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	while (str_piece != NULL)
-	{
-		counter++;
-		str_piece = strtok(NULL, " ");
-	}
-	alloc_words = (char **)malloc(counter * sizeof(char *));
 	dub_str = strdup(str);
 	str_piece = strtok(dub_str, " ");
 
-	while (str_piece != NULL)
+	while(str_piece != NULL)
 	{
-		alloc_words[i] = strdup(str_piece);
-		str_piece = strtok(NULL, " ");
-		i++;
-	}
-	for (i = 0; i < counter; i++)
-	{
-		printf("%s\n" , alloc_words[i]);
+		printf("%s\n", str_piece);
 		str_piece = strtok(NULL, " ");
 	}
-	return (alloc_words);
+	return (0);
 }
