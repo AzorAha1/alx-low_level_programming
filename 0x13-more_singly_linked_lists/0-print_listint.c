95c7c392556578262b05f8347d306142925b41da
AzorAha1 <wizfaiz@icloud.com>
2023-07-31 19:02:59 +0100

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - function name
+ * @h: the argument
+ * Description - this is the description
+ * Return: this is the return type
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t counter;
+	
+	counter = 0;
+	for (; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		counter++;
+	}
+	return (counter);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this is more of the singly linked list, in this project we delve deeper and understand how to use linked lists better in our code
