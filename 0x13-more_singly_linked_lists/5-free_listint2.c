5b24f8ec7642bcb98368a0ddf410b90b5d2f1e49
AzorAha1 <wizfaiz@icloud.com>
2023-07-31 22:34:51 +0100

    free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *newnode;
 	
-	if(*head == NULL)
+	if(*head == NULL || head ==NULL)
 	{
-		return (NULL);
+		return;
 	}
 	while (*head != NULL)
 	{
