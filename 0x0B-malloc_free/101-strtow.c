a570ed4edb83c745a779e281643540988488eeba
AzorAha1 <wizfaiz@icloud.com>
2023-07-31 06:50:37 +0100

    adding dynamic memory allocation to avoid segmentation error

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -11,18 +11,61 @@ char **strtow(char *str)
 {
 	char *dub_str;
 	char *str_piece;
-
+	char **thewords;
+	int  counter;
+	int i;
+	char **alloc;
+	
+	thewords = NULL;
+	counter = 0;
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 	dub_str = strdup(str);
+	if (dub_str == NULL)
+	{
+		return (NULL);
+	}
+	thewords = (char **)malloc(sizeof(char *));
+	if (thewords == NULL)
+	{
+		free(dub_str);
+		return (NULL);
+	}
 	str_piece = strtok(dub_str, " ");
 
 	while (str_piece != NULL)
 	{
-		printf("%s\n", str_piece);
+		char *output = strdup(str_piece);
+		if (output == NULL)
+		{
+			for (i = 0; i < counter; i++)
+			{
+				free(thewords[i]);
+			}
+			free(thewords);
+			free(dub_str);
+			return (NULL);
+		}
+		alloc = realloc(thewords, (counter + 1) * sizeof(char *));
+		if (alloc == NULL)
+		{
+			free(output);
+			for (i = 0; i < counter; i++)
+			{
+				free(thewords[i]);
+			}
+			free(thewords);
+			free(dub_str);
+			return (NULL);
+		}
+		thewords = alloc;
+		thewords[counter] = output;
+		counter++;
 		str_piece = strtok(NULL, " ");
 	}
-	return (0);
+	free(dub_str);
+	thewords = (char **)realloc(thewords, (counter + 1) * sizeof(char *));
+	return (thewords);
 }
