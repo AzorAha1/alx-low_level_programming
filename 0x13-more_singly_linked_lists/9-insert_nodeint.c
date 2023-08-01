d1a5c8eff706db06a1a96501c82394d64e0b01f7
AzorAha1 <wizfaiz@icloud.com>
2023-08-01 22:14:42 +0100

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,6 +41,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
+	if (temp == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
 	newnode->next = temp->next;
 	temp->next = newnode;
 	return (newnode);
