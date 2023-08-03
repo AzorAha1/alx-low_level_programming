92861115a79d73f3e41ac44ebcaad17e02e4a8ba
AzorAha1 <wizfaiz@icloud.com>
2023-08-04 00:22:17 +0100

    checking if b is null

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,6 +17,10 @@ unsigned int binary_to_uint(const char *b)
 	sum = 0;
 	b_len = strlen(b);
 	multiplier = 1;
+	if (b == NULL)
+	{
+		return (0);
+	}
 	for (i = (b_len - 1); i >= 0; i--)
 	{
 		if (b[i] == '1')
