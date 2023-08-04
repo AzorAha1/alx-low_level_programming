d018daf48a8996579eaaadebac9ac3a9e21d7211
AzorAha1 <wizfaiz@icloud.com>
2023-08-04 13:05:36 +0100

    getting nth bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * get_bit - function name
+ * @n: argument
+ * @index: check the index
+ * Description - get the value of a bit at an nth index
+ * Return: return the value
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int value;
+
+	value = (n >> index) & 1;
+
+	return (value);
+}
