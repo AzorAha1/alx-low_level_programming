446251c513de2d393243dc238233438cbb59387f
AzorAha1 <wizfaiz@icloud.com>
2023-08-04 21:45:38 +0100

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,10 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 31)
+	if (index > 63)
 	{
 		return (-1);
 	}
-	*n = *n & (~1 << index);
+	*n = *n & ~(1 << index);
 	return (1);
 }
