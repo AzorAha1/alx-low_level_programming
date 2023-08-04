2a2e8167bf5e1881ad417f0a857bdec12ed3028e
AzorAha1 <wizfaiz@icloud.com>
2023-08-04 15:05:19 +0100

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * clear_bit - name
+ * @n: n
+ * @index: index
+ * Description - this is the description
+ * Return: return type
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = *n & (~1ul << index);
+	return (1);
+}
