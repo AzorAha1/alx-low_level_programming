c4b5bfea7c179abf8bfe9c6ea5aac0d5028016b0
AzorAha1 <wizfaiz@icloud.com>
2023-08-12 02:52:07 +0100

    handling arguments

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,8 @@
 #include <string.h>
 /**
  * main - main function
- * @void: void argument
+ * @argc: argument
+ * @argv: argument
  * Description - this is the description
  * Return: return type
  */
@@ -14,34 +15,24 @@ int main(int argc, char **argv)
 {
 	const char *file_from;
 	const char *file_to;
-	int fp;
-	int fp2;
+	int fp, fp2;
 	char buffer[1024];
 	ssize_t getread, getwrite;
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
+		printf("Usage: cp file_from file_t\n");
 		exit(97);
 	}
 	file_from = argv[1];
 	file_to = argv[2];
-	if (file_from == NULL)
-	{
-		perror("Error: Can't read from file_from\n");
-		exit(98);
-	}
+
 	fp = open(file_from, O_RDONLY);
-	if (fp == -1)
-	{
-		perror("Error: Can't read from the file_to\n");
-		exit (98);
-	}
 	fp2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC);
 	if (fp2 == -1)
 	{
 		perror("Can't write to file_to\n");
-		exit (99);
+		exit(99);
 	}
 	while ((getread = read(fp, buffer, sizeof(buffer))) > 0)
 	{
@@ -61,5 +52,5 @@ int main(int argc, char **argv)
 	}
 	close(fp);
 	close(fp2);
-	return(1);
+	return (1);
 }
