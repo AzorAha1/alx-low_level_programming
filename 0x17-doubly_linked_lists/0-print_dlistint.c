da9eb49b6d3465c7e198bab9bc14d37373c38aec
AzorAha1 <wizfaiz@icloud.com>
2023-09-14 19:23:37 +0100

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements
+ * @h: argument
+ * Description - all elements printed
+ * Return: returns size_t type
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter;
+
+	for (counter = 0; h; h = h->next)
+	{
+		printf("%d\n", h->n);
+		counter++;
+	}
+	return counter;
+}
