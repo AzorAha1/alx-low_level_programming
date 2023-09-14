f550c05963e1aba03cbbf8c56e96702d47f2175d
AzorAha1 <wizfaiz@icloud.com>
2023-09-14 19:47:28 +0100

    setting newnode previous to be null

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	}
 	newnode->n = n;
 	newnode->next = *head;
-	newnnode->prev = NULL;
+	newnode->prev = NULL;
 	*head = newnode;
 	return (newnode);
 }
