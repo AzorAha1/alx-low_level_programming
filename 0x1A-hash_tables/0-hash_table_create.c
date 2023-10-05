228fd0f79ffbd717213ebdffc4c0abec0028c69c
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 00:12:27 +0100

    free if memory allocation to table fails

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = NULL;
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
 	table->size = size;
 	table->array = malloc(sizeof(hash_table_t) * size);
 	if (table->array == NULL)
