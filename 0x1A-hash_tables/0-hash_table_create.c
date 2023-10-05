9eee69e1e4fc81f131823144b9d172e04a17a281
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 00:10:47 +0100

    free table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - function name
- * @size: size
- * Description - creating hash table
- * Return: returns pointer to the new table
- */
+* hash_table_create - function name
+* @size: size
+* Description - creating hash table
+* Return: returns pointer to the new table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
@@ -16,11 +16,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(sizeof(hash_table_t) * size);
 	if (table->array == NULL)
 	{
+		free(table);
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
-		free(table->array[i]);
 		table->array[i] = NULL;
 	}
 	return (table);
