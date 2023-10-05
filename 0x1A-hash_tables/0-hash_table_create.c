05945ad540ca180838a8b9e7aa1eb5e11db49c45
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 00:26:03 +0100

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = NULL;
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	if (!table)
 	{
 		return (NULL);
 	}
 	table->size = size;
-	table->array = malloc(sizeof(hash_table_t) * size);
-	if (table->array == NULL)
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!table->array)
 	{
 		free(table);
 		return (NULL);
