d4ffc8e731bec3ef437543109b785935f0380018
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 20:37:28 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *checker;
 
 	node = NULL;
-	if (key == NULL || ht)
+	if (key == NULL || !ht)
 	{
 		return (0);
 	}
