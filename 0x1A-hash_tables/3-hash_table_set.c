a28bba321116388b4f4a9469cddb00e4878ac993
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 18:28:56 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->next = ht->array[index];
-	node = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
