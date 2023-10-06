adf8a612c6a4d35c5e383b27e2ff75569026fa17
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 21:57:47 +0100

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,6 +11,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	hash_node_t *checker;
 
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
 	index = key_index((const unsigned char *)key, ht->size);
 	checker = ht->array[index];
 	if (!checker)
