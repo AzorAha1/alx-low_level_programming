0b79264b19072773b5430807a38ef1352bb7e8d9
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 21:49:14 +0100

    match is found so we replace the value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - function name
+* @ht: hash table to look into
+* @key: key youre looking for
+* Return: returns the value or null if not key not found_
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index;
+
+	hash_node_t *checker;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	checker = ht->array[index];
+	while (checker != NULL)
+	{
+		if (strcmp(checker->key, key) == 0)
+		{
+			return (checker->value);
+		}
+		return (NULL);
+		checker = checker->next;
+	}
+	return (NULL);
+}
