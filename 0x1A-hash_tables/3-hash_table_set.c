12983a7c13e6ce95f82a6b85764d8db978db6076
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 18:26:52 +0100

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+* hash_table_set - function name
+* @ht: hash table
+* @key: key
+* @value: value
+* Return: 1 on success and 0 otherwise
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index;
+
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (key == NULL)
+	{
+		return (0);
+	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+
+	if (!node->key || !node->value)
+	{
+		free(node);
+		return (0);
+	}
+	node->next = ht->array[index];
+	node = ht->array[index];
+	return (1);
+}
