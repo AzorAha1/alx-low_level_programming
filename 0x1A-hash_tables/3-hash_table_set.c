2b6d168f746514c38abf87e1e46a86203c7a6b40
AzorAha1 <wizfaiz@icloud.com>
2023-10-06 18:48:00 +0100

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,13 +12,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	hash_node_t *node;
 
-	index = key_index((const unsigned char *)key, ht->size);
-	node = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (!node)
+	if (key == NULL)
 	{
 		return (0);
 	}
-	if (key == NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!node)
 	{
 		return (0);
 	}
