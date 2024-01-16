c4b31bfc08d0a0239fd79c363501807e51d4e99a
AzorAha1 <wizfaiz@icloud.com>
2024-01-16 21:17:12 +0100

    1-binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+/**
+* binary_search - function name
+* @array: the array
+* @size: size of array
+* @value: the value to find
+* Description - using binary search to search for value
+* Return: returns int
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left, right, mid, i;
+
+	left = 0;
+	right = size - 1;
+
+	if (!array)
+	{
+		return (-1);
+	}
+	while (left < right)
+	{
+		mid = (left + right) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+			{
+				printf("%d\n", array[i]);
+			}
+			else
+			{
+				printf("%d, ", array[i]);
+			}
+		}
+		if (array[mid] == value)
+		{
+			return (mid);
+		}
+		else if (array[mid] > value)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	return (-1);
+}
